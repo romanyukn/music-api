4586f0e355ead5e4f0ebcf84662d996324aecd79
Natalia Romaniuk <romanyukn@gmail.com>
2021-05-20 15:12:38 -0400

    cards and grid

--- src/components/ArtistsList.js
+++ src/components/ArtistsList.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Table } from 'react-bootstrap';
+import { Table, Card, Row, Col } from 'react-bootstrap';
 import getData from '../services/getData';
 import CountryDropdown from './CountryDropdown';
+import axios from 'axios';
 
 function ArtistsList() {
   const [artistsList, setArtistsList] = useState([]);
@@ -12,34 +13,49 @@ function ArtistsList() {
     const fetchData = async() => {
       const artists = await getData(country || "spain");
       setArtistsList(artists);
+
+      const promises = artistsList.map((artist) => {
+        return axios.get(`http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${artist.name}&api_key=3621bf1f13e493908e1d45a46105db21&page=1&limit=10&format=json`)
+      })
+      
+      const topSongs = (await Promise.all(promises))
+        .map((res) => { 
+          return res.data.toptracks.track 
+        })
+        .map((track) => {
+          const tracks = [];
+          tracks.push(track.name);
+          return tracks;
+        })
+      console.log(topSongs);  
     }
+    
     fetchData();
-  })
+  }, [country])
+  
 
   return(
-    <div>
+    <div className="container">
       <h3 className="mainHeader">Top artists</h3>
       <CountryDropdown />
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Artist Name</th>
-            <th>Listeners</th>
-            <th>Image</th>
-          </tr>
-        </thead>
-        <tbody>
-          {artistsList.map((artist) => 
-            <tr key={artist.mbid}>
-              <td>{artist.id}</td>
-              <td>{artist.name}</td>
-              <td>{artist.listeners}</td>
-              {artist.image.map((el) => el.size === "medium" && <td key={el.size}><img src={el['#text']} alt=""></img></td>)}
-            </tr>
-          )}
-        </tbody>
-      </Table>
+
+      <div className='row'>
+      {artistsList.map((artist) => 
+        <div className="col-sm-3">
+          <Card style={{ width: '15rem', marginBottom: 20 }}>
+            {artist.image.map((el) => el.size === "medium" && 
+              <Card.Img variant="top" src={el['#text']}/>)}
+            <Card.Body>
+              <Card.Title>{artist.name}</Card.Title>
+              <Card.Text>
+                {artist.listeners}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div> 
+      )}
+      </div>
+
     </div>
   )
 }
--- src/services/getData.js
+++ src/services/getData.js
@@ -5,7 +5,7 @@ const apiSecret = "";
 const apiBaseUrl = "http://ws.audioscrobbler.com/2.0/?method="
 
 async function getData(country) {
-  const res = await axios.get(`${apiBaseUrl}geo.gettopartists&country=${country}&api_key=${apiKey}&page=10&limit=10&format=json`);
+  const res = await axios.get(`${apiBaseUrl}geo.gettopartists&country=${country}&api_key=${apiKey}&page=10&limit=16&format=json`);
   return(res.data.topartists.artist);
 }
 
