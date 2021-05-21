5fb423bd2148e4c360044aa8d1fd379c81e9791f
Natalia Romaniuk <romanyukn@gmail.com>
2021-05-21 16:23:45 -0400

    fix getting top tracks for each artist

--- src/components/ArtistsList.js
+++ src/components/ArtistsList.js
@@ -1,38 +1,56 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Table, Card, Row, Col } from 'react-bootstrap';
+import { Table, Card, Row, Col, ListGroup } from 'react-bootstrap';
 import getData from '../services/getData';
 import CountryDropdown from './CountryDropdown';
 import axios from 'axios';
 
 function ArtistsList() {
   const [artistsList, setArtistsList] = useState([]);
+  const [topSongs, setTopSongs] = useState([]);
   let { country } = useParams();
 
   useEffect(() => {
     const fetchData = async() => {
-      const artists = await getData(country || "spain");
-      setArtistsList(artists);
-
-      const promises = artistsList.map((artist) => {
-        return axios.get(`http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${artist.name}&api_key=3621bf1f13e493908e1d45a46105db21&page=1&limit=10&format=json`)
-      })
-      
-      const topSongs = (await Promise.all(promises))
-        .map((res) => { 
-          return res.data.toptracks.track 
+      const artists = (await getData(country || "spain"))
+        .map((ar) => {
+          return {
+            id: ar.mbid,
+            name: ar.name,
+            image: ar.image,
+            listeners: ar.listeners
+          }
         })
-        .map((track) => {
-          const tracks = [];
-          tracks.push(track.name);
-          return tracks;
-        })
-      console.log(topSongs);  
+      setArtistsList(artists); 
     }
-    
     fetchData();
-  }, [country])
-  
+  }, [country]);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const promises = artistsList.map((ar) => {
+        return axios.get(`http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${ar.name}&api_key=3621bf1f13e493908e1d45a46105db21&page=1&limit=10&format=json`);
+      });
+
+      const topSongs = (await Promise.all(promises))
+        .map((res) => {
+          return {
+            artist: res.data.toptracks["@attr"].artist,
+            tracks: res.data.toptracks.track.reduce((accu, track) => {
+              return [
+                ...accu,
+                {
+                  id: track.mbid,
+                  name: track.name
+                }
+              ]
+            }, [])
+          }
+        })
+      setTopSongs(topSongs);  
+    }
+    fetchData();
+  }, [artistsList])
 
   return(
     <div className="container">
@@ -43,14 +61,14 @@ function ArtistsList() {
       {artistsList.map((artist) => 
         <div className="col-sm-3">
           <Card style={{ width: '15rem', marginBottom: 20 }}>
+
             {artist.image.map((el) => el.size === "medium" && 
               <Card.Img variant="top" src={el['#text']}/>)}
             <Card.Body>
-              <Card.Title>{artist.name}</Card.Title>
-              <Card.Text>
-                {artist.listeners}
-              </Card.Text>
+              <Card.Title>{artist.name}</Card.Title>            
+              {topSongs.find((song) => song.artist === artist.name)?.tracks.map(track => <p>{track.name}</p>)}             
             </Card.Body>
+            
           </Card>
         </div> 
       )}
